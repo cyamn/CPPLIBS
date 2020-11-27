fd3131cfd304347d1c77b2b6f6a7c7372c78493c
Ingmar <ingmar@lowack.com>
2020-11-27 20:38:49 +0100

    imported BigDecimal.cc

---
+++ BigDecimal.cc
@@ -0,0 +1,84 @@
+#include <vector>
+#include <cmath>
+#include <ostream>
+#include <limits.h>
+#include <iomanip>
+
+#include <iostream>
+
+unsigned long long upperBound = 10000000000000000;
+
+class bigDecimal{
+  private:
+    std::vector<unsigned long long> dec;
+
+  public:
+    bigDecimal() {}
+
+    bigDecimal(unsigned long long num) {
+      unsigned long long offset, sum;
+      if(num > upperBound) {
+        offset = num / upperBound;
+        sum = num - offset * upperBound;
+        dec.push_back(sum);
+        dec.push_back(offset);
+      } else {
+        dec.push_back(num);
+      }
+    }
+
+    unsigned long long get(unsigned long long index) {
+      return (index > this->length()-1)?0:this->dec[index];
+    }
+
+    bigDecimal operator + (bigDecimal& b) {
+      // variable declaration
+      bigDecimal c = bigDecimal();
+      unsigned long long lA, lB, length, a_part, b_part, sum;
+      unsigned int offset = 0;
+
+      // calculate target length
+      lA = this->length();
+      lB = b.length();
+      length = (lA > lB)? lA: lB;
+
+      // sum a and b up by summing a's and b's parts and
+      // take away an offset
+      for(int i = 0; i < length; i++) {
+        a_part = this->get(i);
+        b_part = b.get(i);
+        sum = a_part+b_part+offset;
+        if(sum > upperBound) {
+          offset = sum /  upperBound;
+          sum -= offset * upperBound;
+        } else {
+          offset = 0;
+        }
+        c.push_back(sum);
+      }
+
+      // make target bigger if necessary
+      if (offset > 0) {
+        c.push_back(offset);
+      }
+      return c;
+    }
+
+
+    // helper mehods
+    void push_back(unsigned long long num){
+        this->dec.push_back(num);
+    }
+    unsigned long long length() {
+      return dec.size();
+    }
+};
+
+// this makes bigDecimal printable
+std::ostream & operator<<(std::ostream &os, bigDecimal &b) {
+      os << b.get(b.length() - 1);
+      for (int i = b.length() - 2; i >= 0; i--) {
+        os << std::setfill('0') << std::setw(16) << b.get(i);
+      }
+      return os;
+}
