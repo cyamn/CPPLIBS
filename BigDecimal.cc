cc4db9b0e9d05424ba93db959259b696d852951e
Ingmar <ingmar@lowack.com>
2020-11-28 18:19:46 +0100

    implemented negative numbers and substraction

--- BigDecimal.cc
+++ BigDecimal.cc
@@ -12,12 +12,30 @@ int bigDecimalLen = 16;
 class bigDecimal{
   private:
     std::vector<unsigned long long> dec;
+    bool negative = false;
+
+    // remove leading cells with zeros
+    void clean() {
+      while (this->dec[this->dec.size()-1] == 0 && this->dec.size() > 1) {
+        this->dec.pop_back();
+      }
+      // -0 := 0
+      if (this->dec.size()==1 && this->dec[0] == 0) {
+        this->negative = false;
+      }
+    }
 
   public:
-    bigDecimal() {}
-
     //split string in reverse into chunks and store them
     bigDecimal(std::string number) {
+      // check if string starts with "-" or "+",...
+      if(number.front() == '-') {
+        this->negative = true;
+        number = number.substr(1);
+      }else if(number.front() == '+'){
+        this->negative = false;
+        number = number.substr(1);
+      }
       int border = number.length();
       int elems = border / bigDecimalLen;
       unsigned long long num;
@@ -29,8 +47,10 @@ class bigDecimal{
       }
     }
 
-    bigDecimal(unsigned long long num) {
-      unsigned long long offset, sum;
+    bigDecimal(long long number) {
+      unsigned long long offset, sum, num;
+      num = (number<0)?-number:number;
+      this->negative=number<0;
       if(num > upperBound) {
         offset = num / upperBound;
         sum = num - offset * upperBound;
@@ -41,8 +61,9 @@ class bigDecimal{
       }
     }
 
-    bigDecimal(std::vector<unsigned long long> dec) {
+    bigDecimal(std::vector<unsigned long long> dec, bool negative = false) {
       this->dec = dec;
+      this->negative= negative;
     }
 
     bigDecimal clone() {
@@ -55,9 +76,9 @@ class bigDecimal{
 
     bigDecimal operator + (bigDecimal& b) {
       // variable declaration
-      bigDecimal c = bigDecimal();
-      unsigned long long lA, lB, length, a_part, b_part, sum;
-      unsigned int offset = 0;
+      bigDecimal c = bigDecimal(this->negative);
+      long long lA, lB, length, a_part, b_part, sum;
+      int offset = 0;
 
       // calculate target length
       lA = this->length();
@@ -68,11 +89,14 @@ class bigDecimal{
       // take away an offset
       for(int i = 0; i < length; i++) {
         a_part = this->get(i);
-        b_part = b.get(i);
+        b_part = (this->negative == b.isNegative())?b.get(i):-b.get(i);
         sum = a_part+b_part+offset;
         if(sum > upperBound) {
           offset = sum /  upperBound;
           sum -= offset * upperBound;
+        } else if(sum < 0){
+          sum = upperBound + sum;
+          offset = -1;
         } else {
           offset = 0;
         }
@@ -82,10 +106,19 @@ class bigDecimal{
       // make target bigger if necessary
       if (offset > 0) {
         c.push_back(offset);
+      } else if (offset < 0){
+        c.push_back(offset);
+        c.setNegative(!c.isNegative());
       }
+      c.clean();
       return c;
     }
     
+    bigDecimal operator - (bigDecimal& b) {
+      bigDecimal minusB(b.dec, !b.negative);
+      return *this + minusB;
+    }
+
     /*
     *   This method returns a shifted big decimal by an index and a chunk and will be used for
     *   multiplikation. e.g: (1234).shift(1) -> (12340)
@@ -139,7 +172,7 @@ class bigDecimal{
       if (newdec.size()==0) {
         newdec.push_back(0);
       }
-      return bigDecimal(newdec);
+      return bigDecimal(newdec, this->negative);
     }
 
     // helper mehods
@@ -149,10 +182,20 @@ class bigDecimal{
     unsigned long long length() {
       return dec.size();
     }
+
+    bool isNegative() {
+      return negative;
+    }
+
+    void setNegative(bool negative) {
+      this->negative = negative;
+    }
+
 };
 
 // this makes bigDecimal printable
 std::ostream & operator<<(std::ostream &os, bigDecimal &b) {
+      if(b.isNegative()) os << "-";
       os << b.get(b.length() - 1);
       for (int i = b.length() - 2; i >= 0; i--) {
         os << std::setfill('0') << std::setw(bigDecimalLen) << b.get(i);
